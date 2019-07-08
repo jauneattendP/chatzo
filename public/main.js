7033b1cbaf25dbb9ab017c045913e85d56e2fe0f
Glitch (socketio-chat-sample) <none>
2019-07-08 03:57:12 +0000

    👨‍❤️‍👨🍘 Checkpoint ./public/main.js:2303429/111 ./public/style.css:2303429/18

--- public/main.js
+++ public/main.js
@@ -1,18 +1,17 @@
 $(function() {
-  var FADE_TIME = 150; // ms
-  var TYPING_TIMER_LENGTH = 400; // ms
+  var FADE_TIME = 150; 
+  var TYPING_TIMER_LENGTH = 400; 
   var COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
   ];
   var $window = $(window);
-  var $usernameInput = $('.usernameInput'); // Input for username
-  var $messages = $('.messages'); // Messages area
-  var $inputMessage = $('.inputMessage'); // Input message input box
-
-  var $loginPage = $('.login.page'); // The login page
-  var $chatPage = $('.chat.page'); // The chatroom page
+  var $usernameInput = $('.usernameInput'); 
+  var $messages = $('.messages'); 
+  var $inputMessage = $('.inputMessage'); 
+  var $loginPage = $('.login.page'); 
+  var $chatPage = $('.chat.page'); 
   var username;
   var connected = false;
   var typing = false;
