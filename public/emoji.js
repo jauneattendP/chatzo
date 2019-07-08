ee1e954984c06f8e7cba94d52c36b9871f876d9d
Glitch (socketio-chat-sample) <none>
2019-07-08 06:42:11 +0000

    🏙👋 Checkpoint ./public/index.html:2303429/456 ./public/emoji.css:2303429/4681

--- public/emoji.js
+++ public/emoji.js
@@ -1,119 +1,132 @@
-import EmojiPicker from "rm-emoji-picker";
- 
-//First construct an instance of EmojiPicker
-const picker = new EmojiPicker();
- 
-//Next tell it where to listen for a click, the container it should be appended to, and the input/textarea/contenteditable it needs to work with
-const icon      = document.getElementById('my-icon');
-const container = document.getElementById('container');
-const editable  = document.getElementById('my-input');
- 
-picker.listenOn(icon, container, editable);
-const text = picker.getText();
-const emoji_text = EmojiPicker.render('lol! :laughing:')
-const picker = new EmojiPicker({
-    sheets: {
-        apple   : '/sheets/sheet_apple_64_indexed_128.png',
-        google  : '/sheets/sheet_google_64_indexed_128.png',
-        twitter : '/sheets/sheet_twitter_64_indexed_128.png',
-        emojione: '/sheets/sheet_emojione_64_indexed_128.png'
+l(function( global ) {
+  var emojify = (function() {
+
+// Get DOM as local variable for simplicity's sake
+var document = global.window.document;
+
+return {
+
+ // This functions sets configuration options
+  config: function(emoticons, people, nature, objects, places, symbols) {
+    this.emoticons_enabled = typeof emoticons !== 'undefined' ? emoticons : true;
+    this.people_enabled = typeof people !== 'undefined' ? people : true;
+    this.nature_enabled = typeof nature !== 'undefined' ? nature : true;
+    this.objects_enabled = typeof objects !== 'undefined' ? objects : true;
+    this.places_enabled = typeof places !== 'undefined' ? places : true;
+    this.symbols_enabled = typeof symbols !== 'undefined' ? symbols : true;
+  },
+
+  // Helper function to find text within DOM
+  findText: function(element, pattern, callback) {
+    for (var childi = element.childNodes.length; childi-->0;) {
+      var child = element.childNodes[childi];
+      if (child.nodeType == 1) {
+
+        // Get tag name and class attribute
+        var tag = child.tagName.toLowerCase(), classname;
+        if(child.hasAttribute('class'))
+          classname = child.getAttribute('class').toLowerCase();
+
+        // Hacky at the minute, needs to be fixed
+        if (classname) {
+           if (tag !== 'script' && tag !== 'style' && tag !== 'textarea' && classname !== 'no-emojify')
+             this.findText(child, pattern, callback);
+        } else {
+           if (tag !== 'script' && tag !== 'style' && tag !== 'textarea')
+             this.findText(child, pattern, callback);
+        }
+
+      } else if (child.nodeType == 3) {
+        var matches = [];
+        if (typeof pattern === 'string') {
+          console.error('Accepts regex only');
+        } else {
+          var match;
+          while (match = pattern.exec(child.data))
+            matches.push(match);
+        }
+        for (var i = matches.length; i-->0;)
+          callback.call(window, child, matches[i]);
+      }
     }
-});
-const picker = new EmojiPicker({
-    //This tells the EmojiPicker that you want to use sprite sheets for operating
-    //systems that don't support emoji (sprite sheets are your fastest option).
-    //I've included sprite sheets for apple, google, twitter, and emojione emojis in the repo.
-    //Feel free to copy those into your web root and provide a path to the files in this option.
-    sheets: {
-        apple   : '/sheets/sheet_apple_64_indexed_128.png',
-        google  : '/sheets/sheet_google_64_indexed_128.png',
-        twitter : '/sheets/sheet_twitter_64_indexed_128.png',
-        emojione: '/sheets/sheet_emojione_64_indexed_128.png'
-    },
-    
-    //Show the colon syntax in the preview or don't. It may not make sense if you're
-    //using a contenteditable element to confuse users with unfamiliar colon syntax
-    show_colon_preview: true,
- 
-    //If you want your contenteditable to be a single-line input, set this to true
-    prevent_new_line : false,
- 
-    //The text that will be displayed when no emoji is being hovered over.
-    default_footer_message: "Please select an emoji from the list above",
- 
-    //Can be "autoplace", "vertical", "horizontal", or a function that takes a tooltip as an argument.
-    //The tooltip is an instance of the class in this repo here: https://github.com/RobertMenke/Tooltip-js
-    positioning: "autoplace",
-    
-    //When the user hovers over the top row of icons, do you want them to be shown
-    //a tooltip indicating which category the icon represents?
-    show_icon_tooltips : true,
- 
-    //Callback that occurs when an emoji gets selected. You get back Emoji, EmojiCategory, Node
-    callback   : (emoji, category, node) => {
-        if(node instanceof HTMLELement){
-            node.classList.add('emoji-image')
-        }
-    },
-    
-    //This optional callback is called any time the picker is opened.
-    onOpen : () => {
-        //trigger some event
-    },
-    
-    //This callback is called once the picker has fully parsed and created markup for each emoji
-    //and emoji category
-    onReady : (categories) => {
-        //example of setting a particular category as active and then filtering its contents
-        categories.setActiveCategoryByName('Activity');
-        picker.active_category.filter((/**Emoji*/emoji) => emoji.matchesSearchTerm(new RegExp("soccer")));
-        //some time later programmatically show all emojis
-        setTimeout(() => {
-            picker.active_category.showAllEmojis();
-        }, 3000)
-    },
- 
-    //Use sprite sheets to display image emojis rather than links to png files (faster).
-    //If you want links to the png files see this repo here for examples (library I'm using):
-    //https://github.com/iamcal/emoji-data
-    use_sheets : true,
-    
-    //By default we show an magnifying glass icon in the search container, 
-    // but if you're not using fontawesome you may want to include your own icon.
-    search_icon : '<i class="fa fa-search" aria-hidden="true"></i>',
-    
-    //Sets of categories and icons that denote sections at the top of the picker.
-    // The category names are not arbitrary, they map to the names of categories in data.js. 
-    // By default, I'm assuming you're using FontAwesome because, well, why wouldn't you?! 
-    // If you want fewer categories, or different icons this is the place to configure that.
-    categories: [
-        {
-            title: "People",
-            icon : '<i class="fa fa-smile-o" aria-hidden="true"></i>'
-        },
-        {
-            title: "Nature",
-            icon : '<i class="fa fa-leaf" aria-hidden="true"></i>'
-        },
-        {
-            title: "Foods",
-            icon : '<i class="fa fa-cutlery" aria-hidden="true"></i>'
-        },
-        {
-            title: "Activity",
-            icon : '<i class="fa fa-futbol-o" aria-hidden="true"></i>'
-        },
-        {
-            title: "Places",
-            icon : '<i class="fa fa-globe" aria-hidden="true"></i>'
-        },
-        {
-            title: "Symbols",
-            icon : '<i class="fa fa-lightbulb-o" aria-hidden="true"></i>'
-        },
-        {
-            title: "Flags",
-            icon : '<i class="fa fa-flag-checkered" aria-hidden="true"></i>'
-        }
-    ]
-});
+  },
+  // Main method
+  run: function() {
+    var emoticons = [
+      [/:-*\)/g, 'emojify blush'],
+      [/:-*o/gi, 'emojify scream'],
+      [/(:|;)-*]/g, 'emojify smirk'],
+      [/(:|;)-*d/gi, 'emojify smiley'],
+      [/xd/gi, 'emojify stuck_out_tongue_closed_eyes'],
+      [/:-*p/gi, 'emojify stuck_out_tongue_winking_eye'],
+      [/:-*(\[|@)/g, 'emojify rage'],
+      [/:-*\(/g, 'emojify disappointed'],
+      [/:'-*\(/g, 'emojify sob'],
+      [/:-*\*/g, 'emojify kissing_heart'],
+      [/;-*\)/g, 'emojify wink'],
+      [/:-*\//g, 'emojify pensive'],
+      [/:-*s/gi, 'emojify confounded'],
+      [/:-*\|/g, 'emojify flushed'],
+      [/:-*\$/g, 'emojify relaxed'],
+      [/:-*x/gi, 'emojify mask'],
+      [/<3/g, 'emojify heart'],
+      [/<\/3/g, 'emojify broken_heart']
+    ], people = [
+      [/:bowtie:/g, 'emojify bowtie'],
+      [/:smile:/g, 'emojify smile'],
+      // Lots more regex keys
+    ], nature = [
+      [/:sunny:/g, 'emojify sunny'],
+      [/:umbrella:/g, 'emojify umbrella'],
+      // Lots more regex keys
+    ], objects = [
+      [/:bamboo:/g, 'emojify bamboo'],
+      [/:gift_heart:/g, 'emojify gift_heart'],
+      // Lots more regex keys
+    ], places = [
+      [/:109:/g, 'emojify onezeronine'],
+      [/:house:/g, 'emojify house'],
+      // Lots more regex keys
+    ], symbols = [
+      [/:one:/g, 'emojify one'],
+      [/:two:/g, 'emojify two'],
+      // Lots more regex keys
+    ], r;
+
+    // Create array of selected icon sets
+    var selected_sets = [];
+    if(this.people_enabled)
+      selected_sets.push(people);
+    if(this.nature_enabled)
+      selected_sets.push(nature);
+    if(this.objects_enabled)
+      selected_sets.push(objects);
+    if(this.places_enabled)
+      selected_sets.push(places);
+    if(this.symbols_enabled)
+      selected_sets.push(symbols);
+    if(this.emoticons_enabled)
+      selected_sets.push(emoticons);
+
+    // Iterate through selected icon sets
+    for (var index = 0; index < selected_sets.length; index++) {
+      // Iterate through all regexes
+      while (r = selected_sets[index].shift()) {
+        // Find and replace matches with <div> tags
+        this.findText(document.body, r[0], function(node, match) {
+          var wrap = document.createElement('div');
+          wrap.setAttribute('class', r[1]);
+          node.splitText(match.index);
+          node.nextSibling.nodeValue = node.nextSibling.nodeValue.substr(match[0].length, node.nextSibling.nodeValue.length);
+          wrap.appendChild(node.splitText(match.index));
+          node.parentNode.insertBefore(wrap, node.nextSibling);
+        });
+      }
+    }
+  }
+};
+  })();
+
+  global.emojify = emojify;
+
+})( this );
