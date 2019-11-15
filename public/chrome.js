9514de22e0afcc626b6e178c9dc41a499e79ba24
Glitch (demo-chatzo) <none>
2019-11-15 18:49:41 +0000

    🐜🍱 Checkpoint ./public/chrome.js:2303429/193 ./public/index.html:2303429/49

---
+++ public/chrome.js
@@ -0,0 +1,2 @@
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if(isChrome){alert('Nous invitons à utiliser Firefox pour naviguer sur ce site: https://firefox.com ')}
