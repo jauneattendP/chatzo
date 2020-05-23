b304c2242de11b6fc9ac864255b05dec9368d158
Glitch (demo-chatzo) <none>
2020-05-23 23:13:10 +0000

    🌲🚍 Updated 0,4

--- public/chrome.js
+++
@@ -1,2 +0,0 @@
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-if(isChrome){alert('Nous invitons à utiliser Firefox pour naviguer sur ce site: https://firefox.com ')}
